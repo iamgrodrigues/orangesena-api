e2a1f067a4cd6de992e446ddec840c8fa49eb1f5
iamgrodrigues <iamgrodrigues@gmail.com>
2021-01-20 12:50:26 -0300

    first commit

---
+++ src/main/java/com/iamgrodrigues/orangesenaapi/OrangeSenaApiApplication.java
@@ -0,0 +1,13 @@
+package com.iamgrodrigues.orangesenaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrangeSenaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrangeSenaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iamgrodrigues/orangesenaapi/OrangeSenaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iamgrodrigues.orangesenaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrangeSenaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
